42cb50bb6c1cb0f30214783bc573d649d78d44b8
exalted .dev22 <exalted.dev22@outlook.com>
2020-02-16 03:16:50 +0500

    Updates

---
+++ precache-manifest.b9138a9b1c6c2c65a93dae0007e136a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1420c26a0e1c57ac8e66cff451c4cd3e",
+    "url": "/todo-list-app/index.html"
+  },
+  {
+    "revision": "8f0522ab73c49a48ef01",
+    "url": "/todo-list-app/static/css/main.6ca741e8.chunk.css"
+  },
+  {
+    "revision": "17a85f080d300264823e",
+    "url": "/todo-list-app/static/js/2.37d52fe0.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/todo-list-app/static/js/2.37d52fe0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f0522ab73c49a48ef01",
+    "url": "/todo-list-app/static/js/main.53813a20.chunk.js"
+  },
+  {
+    "revision": "59f6b2816d940db3cd31",
+    "url": "/todo-list-app/static/js/runtime-main.84cd3415.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-list-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
